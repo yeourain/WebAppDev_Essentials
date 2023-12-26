3b9423dbc04facbd3eef1b6bf1d739118b4e7fdb
TAEHYUN LEE <yeourain@gmail.com>
2023-12-26 14:37:28 +0900

    print

---
+++ java/workspace/print/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author yeourain
+ *
+ */
+module print {
+}
---
+++ java/workspace/print/src/printTest/PrintTest.java
@@ -0,0 +1,10 @@
+package printTest;
+
+public class PrintTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
